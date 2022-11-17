24f6f88abd641a70a6837f50d4b6a220949ec651
Dgon06utrgv <112974161+Dgon06utrgv@users.noreply.github.com>
2022-11-17 21:26:30 +0000

    Web

---
+++ flaskproject/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+
+
+
+
+app = Flask(__name__)
+@app.route("/")
+def main2():
+  return render_template("portfolio example.html")
+
+@app.route("/example")
+def hello_world2():
+    return render_template('base.html')
+  
+@app.route("/<path:filepath>")  
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+
+app.run(host='0.0.0.0',port=8080,debug=False)
